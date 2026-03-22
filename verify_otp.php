dd3d57af0aa3b514130bb7265f1b1b8c52b8c343
parnikamestri <parnikamestri@gmail.com>
2026-03-22 21:23:09 +0530

    Update verify_otp.php

--- verify_otp.php
+++ verify_otp.php
@@ -57,12 +57,12 @@ if(isset($_POST['resend'])){
         $mail->isSMTP();
         $mail->Host       = 'smtp.gmail.com';
         $mail->SMTPAuth   = true;
-        $mail->Username='parnikamestri2026@gmail.com';
-        $mail->Password='gxnf trxi uzex kwxk';
+        $mail->Username='';
+        $mail->Password='';
         $mail->SMTPSecure = 'tls';
         $mail->Port       = 587;
 
-        $mail->setFrom('parnikamestri2026@gmail.com','Student MIS');
+        $mail->setFrom('','Student MIS');
         $mail->addAddress($_SESSION['reset_email']);
 
         $mail->isHTML(true);
